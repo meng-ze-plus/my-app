fa5386bd6a3facb97ccaab48e6c53d25761c4623
meng-ze-plus <47733495+meng-ze-plus@users.noreply.github.com>
2025-10-12 23:57:28 +0800

    Update page.js

--- app/echarts/page.js
+++ app/echarts/page.js
@@ -51,9 +51,9 @@ export default function SearchStudent() {
 
       if (data && data.length > 0) {
         // 按学生去重处理 - 基于姓名、年级和班级
-        //const uniqueStudents = data.reduce((acc, current) => {
+        const uniqueStudents = data.reduce((acc, current) => {
           // 创建唯一标识符：姓名+年级+班级
-          const studentKey = `${current.姓名}-${current.年级}-${current.班级}`;
+          //const studentKey = `${current.姓名}-${current.年级}-${current.班级}`;
           
           // 如果这个学生还没有被添加到结果中
           if (!acc.some(student => 
