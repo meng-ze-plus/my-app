c3028ae1e4f56b5fa20c516b4ac1f155688ff797
meng-ze-plus <2213101331@qq.com>
2025-10-16 16:54:32 +0800

    界面部分汉化

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -17,18 +17,17 @@ export default async function ProtectedPage() {
       <div className="w-full">
         <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
           <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
+          这是一个受保护的页面，只有已认证的用户才能查看
         </div>
       </div>
       <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
+        <h2 className="font-bold text-2xl mb-4">您的用户详情</h2>
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
           {JSON.stringify(data.claims, null, 2)}
         </pre>
       </div>
       <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
+        <h2 className="font-bold text-2xl mb-4">下一步</h2>
         <FetchDataSteps />
       </div>
     </div>
